e233486fc62c2b07805712aa53c0a7041544dcc4
Adetola Adewodu <adetola.adewodu@presencepg.com>
2024-12-23 15:27:44 -0500

    Refactored code

--- etl-geojson.py
+++ etl-geojson.py
@@ -4,6 +4,17 @@ import json
 from geojson import Feature, Point, FeatureCollection, dumps
 
 
+
+def get_station_address_feature(features, data, index, coordinate_key):
+    full_address = data[index][0]["station"] + " " + data[index][0]["unit_id"] 
+    full_address += " acknowledged"
+    longitude = data[index][0]['unit_status'][coordinate_key].get('longitude')
+    latitude = data[index][0]['unit_status'][coordinate_key].get('latitude')
+    my_point = Point((longitude, latitude))
+    temp_feature = Feature(geometry=my_point, properties={"address": full_address})
+    features.append(temp_feature)
+    return features
+
 path = "./data/"
 dir_list = os.listdir(path)
 
@@ -25,74 +36,31 @@ for file in dir_list:
             temp_feature = Feature(geometry=my_point, properties={"address": full_address})
             features.append(temp_feature)
         
-        if i == 'apparatus':
-
-            
+        if i == 'apparatus':            
             status = data[i][0]['unit_status']
             if status.get('acknowledged'):
-                full_address = data[i][0]["station"] + " " + data[i][0]["unit_id"] 
-                full_address += " acknowledged"
-                longitude = data[i][0]['unit_status']['acknowledged'].get('longitude')
-                latitude = data[i][0]['unit_status']['acknowledged'].get('latitude')
-                my_point = Point((longitude, latitude))
-                temp_feature = Feature(geometry=my_point, properties={"address": full_address})
-                features.append(temp_feature)
+                features = get_station_address_feature(features, data, i, 'acknowledged')
 
             if status.get('arrived'):
-                full_address = data[i][0]["station"] + " " + data[i][0]["unit_id"] 
-                full_address += " arrived"
-                longitude = data[i][0]['unit_status']['arrived'].get('longitude')
-                latitude = data[i][0]['unit_status']['arrived'].get('latitude')
-                my_point = Point((longitude, latitude))
-                temp_feature = Feature(geometry=my_point, properties={"address": full_address})
-                features.append(temp_feature)
+                features = get_station_address_feature(features, data, i, 'arrived')
             
             if status.get('available'):
-                full_address = data[i][0]["station"] + " " + data[i][0]["unit_id"] 
-                full_address += " available"
-                longitude = data[i][0]['unit_status']['arrived'].get('longitude')
-                latitude = data[i][0]['unit_status']['arrived'].get('latitude')
-                my_point = Point((longitude, latitude))
-                temp_feature = Feature(geometry=my_point, properties={"address": full_address})
-                features.append(temp_feature)
+                features = get_station_address_feature(features, data, i, 'available')
 
             if status.get('cleared'):
-                full_address = data[i][0]["station"] + " " + data[i][0]["unit_id"] 
-                full_address += " cleared"
-                longitude = data[i][0]['unit_status']['cleared'].get('longitude')
-                latitude = data[i][0]['unit_status']['cleared'].get('latitude')
-                my_point = Point((longitude, latitude))
-                temp_feature = Feature(geometry=my_point, properties={"address": full_address})
-                features.append(temp_feature)
+                features = get_station_address_feature(features, data, i, 'cleared')
             
             if status.get('dispatched'):
-                full_address = data[i][0]["station"] + " " + data[i][0]["unit_id"] 
-                full_address += " dispatched"
-                longitude = data[i][0]['unit_status']['dispatched'].get('longitude')
-                latitude = data[i][0]['unit_status']['dispatched'].get('latitude')
-                my_point = Point((longitude, latitude))
-                temp_feature = Feature(geometry=my_point, properties={"address": full_address})
-                features.append(temp_feature)
+                features = get_station_address_feature(features, data, i, 'dispatched')
 
             if status.get('enroute'):
-                full_address = data[i][0]["station"] + " " + data[i][0]["unit_id"] 
-                full_address += " enroute"
-                longitude = data[i][0]['unit_status']['enroute'].get('longitude')
-                latitude = data[i][0]['unit_status']['enroute'].get('latitude')
-                my_point = Point((longitude, latitude))
-                temp_feature = Feature(geometry=my_point, properties={"address": full_address})
-                features.append(temp_feature)
+                features = get_station_address_feature(features, data, i, 'enroute')
             
             if status.get('~'):
-                full_address = data[i][0]["station"] + " " + data[i][0]["unit_id"] 
-                full_address += " ~"
-                longitude = data[i][0]['unit_status']['~'].get('longitude')
-                latitude = data[i][0]['unit_status']['~'].get('latitude')
-                my_point = Point((longitude, latitude))
-                temp_feature = Feature(geometry=my_point, properties={"address": full_address})
-                features.append(temp_feature)
+                features = get_station_address_feature(features, data, i, '~')
 
 number_of_features = len(features)
+print(number_of_features)
 
 feature_collection = FeatureCollection(features)
 geo_json_output = dumps(feature_collection, sort_keys=True)
